e3ce7c1b900fa0d1e4f06100b92db500035c1194
Brian Maier Jr <brimaidesigns@gmail.com>
2018-11-28 21:20:54 -0500

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task("sass", function() {
     )
     .pipe(prefix(["last 3 versions"], { cascade: true }))
     .pipe(cssnano())
-    .pipe(gulp.dest("_site/assets/css"))
+    .pipe(gulp.dest("_includes"))
     .pipe(browserSync.reload({ stream: true }))
     .pipe(gulp.dest("assets/css"));
 });
